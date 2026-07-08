1b13187c24d322e95208ec514ef5cf125735b69d
Alex Fechet <alex.fechet@softwire.com>
2026-07-08 15:42:45 +0300

    fix_debug_function

--- index.ts
+++ index.ts
@@ -11,8 +11,8 @@ export function fizzbuzz(input : String, params : String[]): String {
     let i :number = Number(input);
     let result : String[] = [];
 
-    if(i % 11 == 0 && params.includes("--11")) {
-        if(i % 13 == 0) {
+    if(i % 11 === 0 && params.includes("--11")) {
+        if(i % 13 === 0) {
             result.push("Fezz");
         }
         result.push("Bong");
@@ -20,25 +20,25 @@ export function fizzbuzz(input : String, params : String[]): String {
         return arrayToString(result);
     }
 
-    if(i % 3 == 0 && params.includes("--3"))
+    if(i % 3 === 0 && params.includes("--3"))
         result.push("Fizz");
-    if(i % 5 == 0 && params.includes("--5"))
+    if(i % 5 === 0 && params.includes("--5"))
         result.push("Buzz");
-    if(i % 7 == 0 && params.includes("--7"))
+    if(i % 7 === 0 && params.includes("--7"))
         result.push("Bang");
 
-    if(i % 13 == 0 && params.includes("--13")) {
+    if(i % 13 === 0 && params.includes("--13")) {
         for(let j : number = 0; j < result.length; j++) {
-            if(result[j]!.at(0) == 'B') {
+            if(result[j]!.at(0) === 'B') {
                 result.splice(j, 0, "Fezz");
             }
         }
     }
 
-    if(i % 17 == 0 && params.includes("--17"))
+    if(i % 17 === 0 && params.includes("--17"))
         result.reverse();
 
-    if(result.length == 0)
+    if(result.length === 0)
         result.push(i.toString());
 
     return arrayToString(result);
@@ -54,12 +54,10 @@ function arrayToString(result :String[]) : String {
 
 
 function debug() : void {
-    if(process.argv.includes("--debug")) {
-        rl.question('Enter your number: ', (userInput) => {
-            console.log(fizzbuzz(userInput, process.argv));
-            rl.close();
-        });
-    }
+    rl.question('Enter your number: ', (userInput) => {
+        console.log(fizzbuzz(userInput, process.argv));
+        rl.close();
+    });
 }
 
 debug();
